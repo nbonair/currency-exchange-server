01120212d033824c29dfe1dfef8f55f6d0e80dba
nbonair <nbholdacc@gmail.com>
2024-10-13 02:06:34 +1100

    Added server run

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,9 @@
 package main
 
-import "github.com/nbonair/currency-exchange-server/internal/www"
+import (
+	"github.com/nbonair/currency-exchange-server/internal/www"
+)
 
 func main() {
-
-	// Load configuration
-	// cfg, err := configs.LoadConfig()
-	// if err != nil {
-	// 	log.Fatalf("Failed to load configuration: %v", err)
-	// }
-
-	// if err := www.Run(cfg); err != nil {
-	// 	fmt.Println(err)
-	// }
 	www.Run()
 }
