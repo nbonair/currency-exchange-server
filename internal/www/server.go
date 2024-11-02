206578d3a87f1503d3366e6777aaf4ce1d1a7aaa
nbonair <nbholdacc@gmail.com>
2024-11-03 05:37:45 +1100

    Added recover for server panic

--- internal/www/server.go
+++ internal/www/server.go
@@ -2,25 +2,34 @@ package www
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/nbonair/currency-exchange-server/configs"
 	"github.com/nbonair/currency-exchange-server/internal/wiring"
 )
 
 func Run() error {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Recovered from panic in Run: %v", r)
+		}
+	}()
+
 	// Load configuration
 	cfg, err := configs.LoadConfig()
 	if err != nil {
-		fmt.Println("Failed to load configuration: %v", err)
+		fmt.Printf("Failed to load configuration: %v", err)
 	}
 
 	r, cleanup, err := wiring.InitializeRouter(cfg.Database, cfg.APIs, cfg.Redis)
 
 	if err != nil {
-		log.Fatalf("Failed to initialize router: %v", err)
+		return fmt.Errorf("failed to initialize router: %w", err)
 	}
 
 	defer cleanup()
-	return r.Run(":8080")
+
+	if err := r.Run(":8080"); err != nil {
+		return fmt.Errorf("server encountered an error: %w", err)
+	}
+	return nil
 }
